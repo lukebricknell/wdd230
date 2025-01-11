0976fac8bf52737d1b2e2e9eeabad5ed4c44d832
lukebricknell <lukecbricknell@gmail.com>
2025-01-11 10:07:02 +0200

    Added last modified

--- week-1/scripts/getDates.js
+++ week-1/scripts/getDates.js
@@ -1,6 +1,9 @@
 const year = document.querySelector("#year");
+const lastMod = document.querySelector("#lastModified");
 
 const today = new Date().getFullYear();
 console.log(today);
 
 year.textContent = today;
+
+lastMod.textContent = document.lastModified;
