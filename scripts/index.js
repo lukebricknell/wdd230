1cd231527dd50b6f50cb429b8fa7c27b5357bf5a
lukebricknell <lukecbricknell@gmail.com>
2025-01-15 19:57:08 +0200

    Added hamburger

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,21 @@ const menuBtn = document.querySelector(".menuBtn");
 
 const navItems = document.querySelectorAll(".nav-li");
 
+const line1 = document.querySelector(".menu-1");
+const line2 = document.querySelector(".menu-2");
+const line3 = document.querySelector(".menu-3");
+
 menuBtn.addEventListener("click", () => {
-  if (navItems.forEach((navItem) => navItem.style.display === "hidden")) {
-    navItems.forEach((navItem) => (navItem.style.display = "inline-block"));
+  console.log(line2.classList);
+  if (line2.classList.contains("hidden")) {
+    navItems.forEach((navItem) => (navItem.style.display = "none"));
+    console.log("close");
   } else {
+    console.log("Open");
     navItems.forEach((navItem) => (navItem.style.display = "inline-block"));
   }
+  line1.classList.toggle("rotate45");
+  line2.classList.toggle("hidden");
+  line3.classList.toggle("rotate-45");
+  console.log(line2.classList);
 });
