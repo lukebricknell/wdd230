4f216c34cdb5362d11e2fd879e98b02d0a83f296
lukebricknell <lukecbricknell@gmail.com>
2025-02-07 19:04:17 +0200

    Links.json

--- scripts/links.js
+++ scripts/links.js
@@ -9,3 +9,37 @@ async function getLinks(linksurl) {
 }
 
 getLinks();
+
+const displayLinks = (links) => {
+  const linkSection = document.querySelector("#link-section");
+
+  links.forEach((item) => {
+    let weekList = document.createElement("li");
+    let week = document.createElement("span");
+
+    week.textContent = item.week + ":";
+
+    weekList.appendChild(week);
+
+    list.forEach((anchor, i) => {
+      const a = document.createElement("a");
+      a.href = anchor.url;
+      if (i.length - 1) {
+        a.textContent = anchor.title;
+      } else {
+        a.textContent = anchor.title + "&nbsp | &nbsp";
+      }
+      weekList.appendChild(a);
+    });
+    linkSection.appendChild(weekList);
+  });
+};
+
+{
+  /* <li>
+              Week 1:
+              <a href="./week-1/chamber-site-plan.html">Layout</a
+              >&nbsp;|&nbsp;<a href="./week-1/media-query.html">Media Query</a
+              >&nbsp;|&nbsp;<a>JS Pen</a>
+            </li> */
+}
